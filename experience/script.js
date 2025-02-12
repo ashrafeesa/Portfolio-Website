8a62ee2d1138ee6ade9e0de8a428e6bc2a76cfd8
Ashraf <ashrafeesa9@gmail.com>
2025-02-12 20:13:46 +0200

    update

--- experience/script.js
+++ experience/script.js
@@ -65,8 +65,8 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Jigar Sable";
-        $("#favicon").attr("href","/assets/images/favicon.png");
+        document.title = "Experience | Portfolio Ashraf Eesa";
+        $("#favicon").attr("href","/assets/images/ashrafeesa.png");
     }
     else {
         document.title = "Come Back To Portfolio";
